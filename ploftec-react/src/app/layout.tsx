94e837dc4df6b07c8159f8d705ef2342149bace5
GonzaIba <gonzaloiba.28.00@gmail.com>
2025-11-25 21:11:15 -0300

    fix: test 6

--- ploftec-react/src/app/layout.tsx
+++ ploftec-react/src/app/layout.tsx
@@ -43,6 +43,12 @@ export default function RootLayout({
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
           crossOrigin="anonymous"
         />*/}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
         <link
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
